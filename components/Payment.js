d7ba46e8450598a8dfdd30a79792d99223f9e9db
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-20 00:42:08 -0400

    updated props, added calculation of payment due date and calculation of invoice item price totals

--- components/Payment.js
+++ components/Payment.js
@@ -1,18 +1,46 @@
+import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
-export default function Payment({ id, clientName, createdAt, total, status }) {
-  const currentStatus = status[0].toUpperCase() + status.slice(1);
+export default function Payment({
+  id,
+  clientName,
+  invoiceDate,
+  paymentTerms,
+  items,
+  status,
+}) {
+  const date = new Date(invoiceDate);
 
-  const date = new Date(createdAt);
+  let paymentPeriod;
+
+  if (paymentTerms === 'Net 1 Day') {
+    paymentPeriod = 1;
+  } else if (paymentTerms === 'Net 7 Days') {
+    paymentPeriod = 7;
+  } else if (paymentTerms === 'Net 14 Days') {
+    paymentPeriod = 14;
+  } else {
+    paymentPeriod = 30;
+  }
+
+  const paymentDueDate = new Date(
+    date.getTime() + paymentPeriod * 25 * 60 * 60 * 1000
+  );
+
   const options = {
     day: 'numeric',
     month: 'short',
     year: 'numeric',
   };
 
-  const formattedDate = date.toLocaleDateString('en-US', options);
+  const formattedDate = paymentDueDate.toLocaleDateString('en-US', options);
 
+  const totals = Array.from(items).reduce((acc, curr) => {
+    return acc + Number(curr.total);
+  }, 0);
+
+  const currentStatus = status[0].toLowerCase() + status.slice(1);
   const statusColours = {
     paid: 'bg-bgPaid text-paid',
     pending: 'bg-bgPending text-pending',
@@ -21,30 +49,32 @@ export default function Payment({ id, clientName, createdAt, total, status }) {
 
   return (
     <div className="bg-mainPurple flex flex-col gap-6 rounded-lg px-6 py-6">
-      <div className="flex justify-between">
-        <span className="font-medium">
-          <span className="text-lightPurple">#</span>
-          {id}
-        </span>
-        <p className="font-light">{clientName}</p>
-      </div>
-
-      <div className="flex justify-between items-center">
-        <div>
-          <p className="font-light">Due {formattedDate}</p>
-          <span className="text-lg font-medium">${total}</span>
+      <Link href={`/${id}`}>
+        <div className="flex justify-between">
+          <span className="font-medium">
+            <span className="text-lightPurple">#</span>
+            {id.slice(-6)}
+          </span>
+          <p className="font-light">{clientName}</p>
         </div>
 
-        <div
-          className={`${statusColours[status]} flex items-center gap-2 rounded-md py-2.5 px-7`}
-        >
-          <FontAwesomeIcon
-            icon={faCircle}
-            className="text-[10px]"
-          ></FontAwesomeIcon>
-          <p className="font-medium pt-[1.5px]">{currentStatus}</p>
+        <div className="flex justify-between items-center">
+          <div>
+            <p className="font-light">Due {formattedDate}</p>
+            <span className="text-lg font-medium">${totals.toFixed(2)}</span>
+          </div>
+
+          <div
+            className={`${statusColours[currentStatus]} flex items-center gap-2 rounded-md py-2.5 px-7`}
+          >
+            <FontAwesomeIcon
+              icon={faCircle}
+              className="text-[10px]"
+            ></FontAwesomeIcon>
+            <p className="font-medium pt-[1.5px]">{status}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
