91ec55387df2bf39ad71d8a03a920cdddef37930
Nesha Mervin <nesha.mervin@gmail.com>
2023-04-14 14:30:13 -0400

    created page for rendering selected payment details. connected to the databased to render dynamic pages with the data

---
+++ pages/[paymentId].js
@@ -0,0 +1,66 @@
+import { MongoClient, ObjectId } from 'mongodb';
+import PaymentDetail from '../components/PaymentDetail';
+
+export default function PaymentDetails(props) {
+  return (
+    <>
+      <PaymentDetail paymentData={props} />
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
+
+  const db = client.db();
+  const invoicesCollections = db.collection('invoices');
+
+  const invoices = await invoicesCollections.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: false,
+    paths: invoices.map(invoice => ({
+      params: { paymentId: invoice._id.toString() },
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  const paymentId = context.params.paymentId;
+
+  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
+
+  const db = client.db();
+  const invoicesCollections = db.collection('invoices');
+
+  const selectedPayment = await invoicesCollections.findOne({
+    _id: new ObjectId(paymentId),
+  });
+
+  client.close();
+
+  return {
+    props: {
+      paymentData: {
+        id: selectedPayment._id.toString(),
+        street: selectedPayment.street,
+        city: selectedPayment.city,
+        postal: selectedPayment.postal,
+        country: selectedPayment.country,
+        clientName: selectedPayment.clientName,
+        clientEmail: selectedPayment.clientEmail,
+        clientStreet: selectedPayment.clientStreet,
+        clientCity: selectedPayment.clientCity,
+        clientPostal: selectedPayment.clientPostal,
+        clientCountry: selectedPayment.clientCountry,
+        invoiceDate: selectedPayment.invoiceDate,
+        paymentTerms: selectedPayment.paymentTerms,
+        description: selectedPayment.description,
+        status: selectedPayment.status,
+        items: selectedPayment.items,
+      },
+    },
+  };
+}
