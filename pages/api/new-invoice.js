01d3e9117522a674ff7b1b2bf703cb3638ad5e98
Nesha Mervin <nesha.mervin@gmail.com>
2023-04-18 18:55:36 -0400

    refactored existing code to connect to MongoDB and set up conditional for handling PUT request to update invoice status from Draft to Pending in database

--- pages/api/new-invoice.js
+++ pages/api/new-invoice.js
@@ -1,19 +1,32 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 export default async function handler(req, res) {
+  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
+
+  const db = client.db();
+  const invoicesCollections = db.collection('invoices');
+
   if (req.method === 'POST') {
     const data = req.body;
-
-    const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
-
-    const db = client.db();
-    const invoicesCollections = db.collection('invoices');
 
     const result = await invoicesCollections.insertOne(data);
     console.log(result);
 
-    client.close();
-
     res.status(201).json({ message: 'Invoice inserted!' });
+  } else if (req.method === 'PUT') {
+    const id = req.body;
+
+    const result = await invoicesCollections.updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+      { $set: { status: 'Pending' } }
+    );
+
+    console.log(result);
+
+    res.status(200).json({ message: 'Payment status updated!' });
   }
+
+  client.close();
 }
