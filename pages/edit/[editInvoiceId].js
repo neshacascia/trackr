b2841cf14553de6d4e9ea145ca70355ae310eda3
Nesha Mervin <nesha.mervin@gmail.com>
2023-04-17 22:21:32 -0400

    set up dynamic page for rendering edit invoice page. set up functions for locating correct invoice and its appropriate form data in mongodb based on the params

---
+++ pages/edit/[editInvoiceId].js
@@ -0,0 +1,94 @@
+import { MongoClient, ObjectId } from 'mongodb';
+import { useRouter } from 'next/router';
+import EditInvoiceForm from '@/components/EditInvoiceForm';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+
+export default function EditInvoice(props) {
+  const router = useRouter();
+
+  async function updateInvoiceHandler(enteredInvoiceData) {
+    router.back();
+  }
+
+  return (
+    <main className="bg-darkPurple h-full flex flex-col gap-6 px-6">
+      <button
+        onClick={() => router.back()}
+        className="flex items-center gap-6 pt-8"
+      >
+        <FontAwesomeIcon
+          icon={faAngleLeft}
+          className="text-brightPurple text-lg"
+        ></FontAwesomeIcon>{' '}
+        <p className="text-white font-medium pt-[2px]">Go back</p>
+      </button>
+
+      <h2 className="text-white text-3xl">
+        Edit #{props.paymentData.id.slice(-6).toUpperCase()}
+      </h2>
+
+      <EditInvoiceForm
+        updateInvoice={updateInvoiceHandler}
+        invoiceData={props.paymentData}
+      />
+    </main>
+  );
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
+
+  const db = client.db();
+  const invoicesCollections = db.collection('invoices');
+
+  const invoices = await invoicesCollections.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: false,
+    paths: invoices.map(invoice => ({
+      params: { editInvoiceId: invoice._id.toString() },
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  const { editInvoiceId } = context.params;
+
+  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
+
+  const db = client.db();
+  const invoicesCollections = db.collection('invoices');
+
+  const selectedPayment = await invoicesCollections.findOne({
+    _id: new ObjectId(editInvoiceId),
+  });
+
+  client.close();
+
+  return {
+    props: {
+      paymentData: {
+        id: selectedPayment._id.toString(),
+        street: selectedPayment.street,
+        city: selectedPayment.city,
+        postal: selectedPayment.postal,
+        country: selectedPayment.country,
+        clientName: selectedPayment.clientName,
+        clientEmail: selectedPayment.clientEmail,
+        clientStreet: selectedPayment.clientStreet,
+        clientCity: selectedPayment.clientCity,
+        clientPostal: selectedPayment.clientPostal,
+        clientCountry: selectedPayment.clientCountry,
+        invoiceDate: selectedPayment.invoiceDate,
+        paymentTerms: selectedPayment.paymentTerms,
+        description: selectedPayment.description,
+        status: selectedPayment.status,
+        items: selectedPayment.items,
+      },
+    },
+  };
+}
