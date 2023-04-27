797cd603d1937b59ee7eb3215be69c18e757e586
Nesha Mervin <nesha.mervin@gmail.com>
2023-04-27 16:13:03 -0400

    added styling to render modal on top of all other components, styled links

--- components/MobileMenuModal.js
+++ components/MobileMenuModal.js
@@ -8,23 +8,30 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 export default function MobileMenuModal() {
   const { toggleMenu } = useContext(Context);
 
+  console.log(toggleMenu);
+
   return (
     <>
       {toggleMenu && (
-        <div>
-          <FontAwesomeIcon icon={faXmark}></FontAwesomeIcon>
+        <div className="bg-[rgba(0,0,0,0.4)] h-screen fixed inset-0 z-50">
+          <div className="text-white bg-mainPurple w-[60%] h-screen flex flex-col justify-center items-center">
+            <FontAwesomeIcon
+              icon={faXmark}
+              className="text-xl absolute right-[45%] top-[5%]"
+            ></FontAwesomeIcon>
 
-          <ul>
-            <li>
-              <Link href="/">home</Link>
-            </li>
-            <li>
-              <Link href="/invoices">invoices</Link>
-            </li>
-            <li>
-              <Link href="/expenses">expenses</Link>
-            </li>
-          </ul>
+            <ul className="text-2xl flex flex-col gap-6">
+              <li>
+                <Link href="/">home</Link>
+              </li>
+              <li>
+                <Link href="/invoices">invoices</Link>
+              </li>
+              <li>
+                <Link href="/expenses">expenses</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       )}
     </>
