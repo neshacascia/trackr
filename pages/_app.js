2ca2878241addf1cadfa25873438756e9dc7e92e
Nesha Mervin <nesha.mervin@gmail.com>
2023-04-27 16:01:00 -0400

    wrapped Component around ContextProvider and rendered MobileMenuModal component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
+import { ContextProvider } from '../components/context/StateContext';
+
 import Layout from '@/components/Layout';
+import MobileMenuModal from '@/components/MobileMenuModal';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+        <MobileMenuModal />
+      </Layout>
+    </ContextProvider>
   );
 }
