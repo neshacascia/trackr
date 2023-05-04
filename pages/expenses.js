ee379e6d15bedbd5e872cd2aaf41858e04d16f73
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-04 13:22:39 -0400

    created expenses page with dummy data to render components

---
+++ pages/expenses.js
@@ -0,0 +1,28 @@
+import { useContext } from 'react';
+import { Context } from '@/components/context/StateContext';
+import Header from '@/components/Header';
+import PaymentsList from '@/components/PaymentsList';
+
+const expenses = [
+  {
+    id: 'abcd7aj924',
+    item: 'Gas',
+    amount: 100.0,
+    expenseDue: '2021-08-19',
+  },
+];
+
+export default function Expenses() {
+  const { isDarkMode } = useContext(Context);
+
+  return (
+    <main
+      className={`${
+        isDarkMode ? 'text-white bg-darkPurple' : 'text-lightText bg-lightBg'
+      } font-spartan h-screen w-full flex flex-col items-center gap-8 pt-8`}
+    >
+      <Header title="Expenses" payments={expenses} />
+      <PaymentsList type="expenses" expenses={expenses} />
+    </main>
+  );
+}
