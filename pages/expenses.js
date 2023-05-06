bcffd03ba5832dcb1d0e92d50958f76413ee79b9
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-06 12:01:16 -0400

    export async function getStaticProps to retrieve the expenses data stored in mongodb databases and map it to specific properties in an object that will get passed as props to the Expenses component

--- pages/expenses.js
+++ pages/expenses.js
@@ -1,18 +1,10 @@
+import { MongoClient } from 'mongodb';
 import { useContext } from 'react';
 import { Context } from '@/components/context/StateContext';
 import Header from '@/components/Header';
 import PaymentsList from '@/components/PaymentsList';
 
-const expenses = [
-  {
-    id: 'abcd7aj924',
-    expenseName: 'Gas',
-    amount: 100.0,
-    expenseDue: '2021-08-19',
-  },
-];
-
-export default function Expenses() {
+export default function Expenses(props) {
   const { isDarkMode } = useContext(Context);
 
   return (
@@ -21,8 +13,37 @@ export default function Expenses() {
         isDarkMode ? 'text-white bg-darkPurple' : 'text-lightText bg-lightBg'
       } font-spartan h-screen w-full flex flex-col items-center gap-8 pt-8`}
     >
-      <Header title="Expenses" payments={expenses} />
-      <PaymentsList type="expenses" expenses={expenses} />
+      <Header title="Expenses" payments={props.expenses} />
+      <PaymentsList type="expenses" expenses={props.expenses} />
     </main>
   );
+}
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
+
+  const db = client.db();
+  const expensesCollections = db.collection('expenses');
+
+  const expenses = await expensesCollections.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      expenses: expenses.map(expense => ({
+        id: expense._id.toString(),
+        merchant: expense.merchant,
+        merchantEmail: expense.merchantEmail,
+        referenceNo: expense.referenceNo,
+        accountNo: expense.accountNo,
+        expenseAmount: expense.expenseAmount,
+        expenseDueDate: expense.expenseDueDate,
+        expenseCategory: expense.expenseCategory,
+        notes: expense.notes,
+        status: expense.status,
+      })),
+    },
+    revalidate: 30,
+  };
 }
