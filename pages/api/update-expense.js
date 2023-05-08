31ac50b29bb877d5534a55a92ccf47fdc15ea474
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-08 02:27:27 -0400

    implemented PUT route for updating expense data in the database. uses updateOne method to find the correct document with the specified ID, replacing the old data with the new

--- pages/api/update-expense.js
+++ pages/api/update-expense.js
@@ -26,5 +26,16 @@ export default async function handler(req, res) {
     console.log(result);
 
     res.status(200).json({ message: 'Expense deleted!' });
+  } else if (req.method === 'PUT') {
+    const { id, ...updatedData } = req.body;
+
+    const result = await expensesCollections.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updatedData }
+    );
+    console.log(result);
+
+    res.status(201).json({ message: 'Expense updated!' });
   }
+  client.close();
 }
