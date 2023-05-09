5c571933add384a71ceddb41b3e66e5a200364aa
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-09 16:45:45 -0400

    increased top padding for tablet devices

--- pages/invoices/index.js
+++ pages/invoices/index.js
@@ -11,7 +11,7 @@ export default function Invoices(props) {
     <main
       className={`${
         isDarkMode ? 'text-white bg-darkPurple' : 'text-lightText bg-lightBg'
-      } font-spartan h-screen w-full flex flex-col items-center gap-8 pt-8`}
+      } font-spartan h-screen w-full flex flex-col items-center gap-8 pt-8 md:pt-14`}
     >
       <Header title="Invoices" payments={props.invoices} />
       <PaymentsList type="invoices" invoices={props.invoices} />
