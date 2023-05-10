fb77f5917b0d33bc4285fe89a0be154f91b58fcb
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-10 16:22:01 -0400

    updated padding top to height of navbar to accomodate the fixed navbar

--- pages/expenses/new-expense.js
+++ pages/expenses/new-expense.js
@@ -30,7 +30,7 @@ export default function NewExpense() {
     <main
       className={`${
         isDarkMode ? 'bg-darkPurple' : 'bg-lightBg'
-      } h-full flex flex-col gap-6 px-6`}
+      } h-full flex flex-col gap-6 px-6 pt-[72px]`}
     >
       <button
         onClick={() => router.push('/expenses')}
