69925fe1fb38abd931ef42645c1d82403f8cbe8a
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-16 00:12:54 -0400

    increased right padding

--- components/DataStats.js
+++ components/DataStats.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function DataStats({ icon, total, emails, title }) {
   return (
-    <div className="bg-mainPurple h-32 flex justify-between gap-6 rounded-lg py-8 pl-6 pr-20">
+    <div className="bg-mainPurple h-32 flex justify-between gap-6 rounded-lg py-8 pl-6 pr-24">
       <FontAwesomeIcon
         icon={icon}
         className="text-grayerPurple bg-borderPurple text-xl p-4 rounded-full"
