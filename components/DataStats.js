7a7dc0af44eeb5f581e196ad18f756a891ba63d7
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-16 00:25:36 -0400

    added light mode styling

--- components/DataStats.js
+++ components/DataStats.js
@@ -1,18 +1,35 @@
+import { useContext } from 'react';
+import { Context } from './context/StateContext';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function DataStats({ icon, total, emails, title }) {
+  const { isDarkMode } = useContext(Context);
+
   return (
-    <div className="bg-mainPurple h-32 flex justify-between gap-6 rounded-lg py-8 pl-6 pr-24">
+    <div
+      className={`${
+        isDarkMode ? 'bg-mainPurple' : 'bg-white'
+      } h-32 flex justify-between gap-6 rounded-lg py-8 pl-6 pr-24`}
+    >
       <FontAwesomeIcon
         icon={icon}
-        className="text-grayerPurple bg-borderPurple text-xl p-4 rounded-full"
+        className={`text-grayerPurple ${
+          isDarkMode ? 'bg-borderPurple' : 'bg-[#ededf1]'
+        } text-xl p-4 rounded-full`}
       />
 
       <div className="min-w-full flex flex-col gap-2">
-        <p className="text-white text-2xl">
+        <p className={`${isDarkMode ? 'text-white' : 'text-black'} text-2xl`}>
           {total ? `$${total.toFixed(2)}` : emails}
         </p>
-        <p className="text-draft font-light">Total {title}</p>
+        <p
+          className={`${
+            isDarkMode ? 'text-draft' : 'text-detailPurple'
+          } font-light`}
+        >
+          Total {title}
+        </p>
       </div>
     </div>
   );
