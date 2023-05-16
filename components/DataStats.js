f2626d759962a34d61250235a9c0b08ab0b5c466
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-16 14:10:25 -0400

    added full width to div

--- components/DataStats.js
+++ components/DataStats.js
@@ -10,7 +10,7 @@ export default function DataStats({ icon, total, emails, title }) {
     <div
       className={`${
         isDarkMode ? 'bg-mainPurple' : 'bg-white'
-      } h-32 flex justify-between gap-6 rounded-lg py-8 pl-6 pr-24`}
+      } w-full h-32 flex justify-between gap-6 rounded-lg py-8 pl-6 pr-24`}
     >
       <FontAwesomeIcon
         icon={icon}
