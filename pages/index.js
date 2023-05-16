f6f660f6426d56fb234d610963ad9e66f3563832
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-15 23:28:18 -0400

    implemented getStaticProps to fetch invoices and expenses from the database, and pass each item's total amount down as props

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { MongoClient } from 'mongodb';
 import Head from 'next/head';
 import { useContext } from 'react';
 import { Context } from '@/components/context/StateContext';
@@ -8,7 +9,7 @@ import {
   faReceipt,
 } from '@fortawesome/free-solid-svg-icons';
 
-export default function Home() {
+export default function Home(props) {
   const { isDarkMode } = useContext(Context);
 
   return (
@@ -25,10 +26,44 @@ export default function Home() {
         } w-screen h-screen flex px-6 md:px-12 xl:px-[252px]`}
       >
         <section className="flex gap-4 pt-[72px] mt-8 md:mt-14">
-          <DataStats title="Invoices" icon={faMoneyBillTrendUp} />
-          <DataStats title="Expenses" icon={faReceipt} />
+          <DataStats
+            title="Invoices"
+            icon={faMoneyBillTrendUp}
+            total={props.invoicesStats}
+          />
+          <DataStats
+            title="Expenses"
+            icon={faReceipt}
+            total={props.expensesStats}
+          />
         </section>
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
+
+  const db = client.db();
+
+  const invoicesCollections = db.collection('invoices');
+  const invoices = await invoicesCollections.find().toArray();
+
+  const expensesCollections = db.collection('expenses');
+  const expenses = await expensesCollections.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      invoicesStats: invoices.map(invoice => ({
+        total: invoice.items.map(item => item.total),
+      })),
+      expensesStats: expenses.map(expense => ({
+        amount: expense.expenseAmount,
+      })),
+    },
+    revalidate: 5,
+  };
 }
