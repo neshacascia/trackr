a4f9edf6f12961fa4be4a494a2e02f74a5df870e
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-15 23:43:34 -0400

    destructured the props, and used toFixed method to total

--- components/DataStats.js
+++ components/DataStats.js
@@ -1,16 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function DataStats(props) {
+export default function DataStats({ icon, total, title }) {
   return (
     <div className="bg-mainPurple h-32 flex justify-between gap-6 rounded-lg py-8 pl-6 pr-20">
       <FontAwesomeIcon
-        icon={props.icon}
+        icon={icon}
         className="text-grayerPurple bg-borderPurple text-xl p-4 rounded-full"
       />
 
       <div className="min-w-full flex flex-col gap-2">
-        <p className="text-white text-2xl">${}</p>
-        <p className="text-draft font-light">Total {props.title}</p>
+        <p className="text-white text-2xl">${total.toFixed(2)}</p>
+        <p className="text-draft font-light">Total {title}</p>
       </div>
     </div>
   );
