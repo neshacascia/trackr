5c72fc32aef43dd59cbeac301e9575ddd2901111
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-16 00:06:19 -0400

    conditional render dollar sign if total prop is available otherwise render emails prop

--- components/DataStats.js
+++ components/DataStats.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function DataStats({ icon, total, title }) {
+export default function DataStats({ icon, total, emails, title }) {
   return (
     <div className="bg-mainPurple h-32 flex justify-between gap-6 rounded-lg py-8 pl-6 pr-20">
       <FontAwesomeIcon
@@ -9,7 +9,9 @@ export default function DataStats({ icon, total, title }) {
       />
 
       <div className="min-w-full flex flex-col gap-2">
-        <p className="text-white text-2xl">${total.toFixed(2)}</p>
+        <p className="text-white text-2xl">
+          {total ? `$${total.toFixed(2)}` : emails}
+        </p>
         <p className="text-draft font-light">Total {title}</p>
       </div>
     </div>
