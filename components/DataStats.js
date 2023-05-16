5b55cc78f10a30c96d85a203b15cd23afe018ba7
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-15 23:07:57 -0400

    removed div and added background colour to icon

--- components/DataStats.js
+++ components/DataStats.js
@@ -2,15 +2,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function DataStats(props) {
   return (
-    <div className="bg-mainPurple h-32 flex justify-between gap-6 rounded-lg py-8 pl-8 pr-20">
-      <div className="bg-borderPurple w-12 h-12 flex items-center justify-center rounded-full p-3">
-        <FontAwesomeIcon
-          icon={props.icon}
-          className="text-grayerPurple text-xl"
-        />
-      </div>
+    <div className="bg-mainPurple h-32 flex justify-between gap-6 rounded-lg py-8 pl-6 pr-20">
+      <FontAwesomeIcon
+        icon={props.icon}
+        className="text-grayerPurple bg-borderPurple text-xl p-4 rounded-full"
+      />
 
-      <div className="flex flex-col gap-2">
+      <div className="min-w-full flex flex-col gap-2">
         <p className="text-white text-2xl">${}</p>
         <p className="text-draft font-light">Total {props.title}</p>
       </div>
