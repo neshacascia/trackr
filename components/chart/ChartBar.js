f86b1537f3b69d54f45b2b0c9ab6a9536918fb2e
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-16 15:38:51 -0400

    implemented tooltip styling for filled chart bars to display the total invoice amount made when hovering over a specific month's bar

--- components/chart/ChartBar.js
+++ components/chart/ChartBar.js
@@ -5,6 +5,7 @@ export default function ChartBar(props) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
   }
 
+  console.log(props.value);
   return (
     <div className="h-full flex flex-col items-center gap-2">
       <div
@@ -15,9 +16,13 @@ export default function ChartBar(props) {
         } h-full w-4 flex flex-col justify-end border-[1px] rounded-md overflow-hidden`}
       >
         <div
-          className="chart-bar bg-brightPurple w-full"
+          className="chart-bar bg-brightPurple w-full inline-block"
           style={{ height: barFillHeight }}
-        ></div>
+        >
+          <span className="tooltiptext text-white bg-darkPurple text-sm text-center max-w-full absolute rounded-md p-2 ml-1 z-50">
+            {`$${props.value.toFixed(2)}`}
+          </span>
+        </div>
       </div>
       <div className="font-bold text-sm text-center">{props.label}</div>
     </div>
