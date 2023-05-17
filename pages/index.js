13fe0b38fe9785c3b1086c1c803cfd1989e3463e
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-17 00:06:04 -0400

    set smaller gap to mobile devices

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home(props) {
           isDarkMode ? 'bg-darkPurple' : 'bg-lightBg'
         } w-screen h-full flex flex-col gap-6 px-6 pb-8 md:h-screen md:justify-center md:gap-0 md:px-12 md:pb-0 xl:px-[252px]`}
       >
-        <section className="w-full flex flex-col items-center gap-10">
+        <section className="w-full flex flex-col items-center gap-4 md:gap-10">
           <section className="h-[40%] flex flex-col items-center gap-4 pt-[72px] mt-8 md:w-full md:flex-row md:justify-between md:pt-0 md:mt-2">
             <DataStats
               title="Invoices"
