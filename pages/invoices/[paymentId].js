eab18a864f4e3e6c47b6581ad00f7b6382ae0e0c
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-18 00:20:26 -0400

    switched to getServerSideProps to see if page will be generated on each request, fetching the latest data from the server

--- pages/invoices/[paymentId].js
+++ pages/invoices/[paymentId].js
@@ -35,25 +35,7 @@ export default function PaymentDetails(props) {
   );
 }
 
-export async function getStaticPaths() {
-  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
-
-  const db = client.db();
-  const invoicesCollections = db.collection('invoices');
-
-  const invoices = await invoicesCollections.find({}, { _id: 1 }).toArray();
-
-  client.close();
-
-  return {
-    fallback: 'blocking',
-    paths: invoices.map(invoice => ({
-      params: { paymentId: invoice._id.toString() },
-    })),
-  };
-}
-
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
   const paymentId = context.params.paymentId;
 
   const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
@@ -88,6 +70,62 @@ export async function getStaticProps(context) {
         items: selectedPayment.items,
       },
     },
-    revalidate: 10,
   };
 }
+
+// export async function getStaticPaths() {
+//   const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
+
+//   const db = client.db();
+//   const invoicesCollections = db.collection('invoices');
+
+//   const invoices = await invoicesCollections.find({}, { _id: 1 }).toArray();
+
+//   client.close();
+
+//   return {
+//     fallback: 'blocking',
+//     paths: invoices.map(invoice => ({
+//       params: { paymentId: invoice._id.toString() },
+//     })),
+//   };
+// }
+
+// export async function getStaticProps(context) {
+//   const paymentId = context.params.paymentId;
+
+//   const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
+
+//   const db = client.db();
+//   const invoicesCollections = db.collection('invoices');
+
+//   const selectedPayment = await invoicesCollections.findOne({
+//     _id: new ObjectId(paymentId),
+//   });
+
+//   client.close();
+
+//   return {
+//     props: {
+//       paymentData: {
+//         id: selectedPayment._id.toString(),
+//         street: selectedPayment.street,
+//         city: selectedPayment.city,
+//         postal: selectedPayment.postal,
+//         country: selectedPayment.country,
+//         clientName: selectedPayment.clientName,
+//         clientEmail: selectedPayment.clientEmail,
+//         clientStreet: selectedPayment.clientStreet,
+//         clientCity: selectedPayment.clientCity,
+//         clientPostal: selectedPayment.clientPostal,
+//         clientCountry: selectedPayment.clientCountry,
+//         invoiceDate: selectedPayment.invoiceDate,
+//         paymentTerms: selectedPayment.paymentTerms,
+//         description: selectedPayment.description,
+//         status: selectedPayment.status,
+//         items: selectedPayment.items,
+//       },
+//     },
+//     revalidate: 10,
+//   };
+// }
