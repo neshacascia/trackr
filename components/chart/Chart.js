29aae71b5ff8b3e42cfe8af6c171a2885602ae1a
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-26 14:39:27 -0400

    added legend to assist users in identifying invoice and expense chart bars by colour

--- components/chart/Chart.js
+++ components/chart/Chart.js
@@ -1,5 +1,8 @@
 import ChartBar from './ChartBar';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquare } from '@fortawesome/free-solid-svg-icons';
+
 export default function Chart(props) {
   const dataPointValues = props.dataPoints.map(
     dataPoint => dataPoint.invoiceValue
@@ -17,19 +20,39 @@ export default function Chart(props) {
         props.isDarkMode
           ? 'text-draft bg-mainPurple'
           : 'text-grayerPurple bg-[#fdfdfd]'
-      } text-center w-full h-72 flex justify-around rounded-lg px-4 py-6`}
+      } text-center w-full h-72 flex flex-col gap-4 rounded-lg px-4 py-6`}
     >
-      {props.dataPoints.map(dataPoint => (
-        <ChartBar
-          key={dataPoint.label}
-          value={dataPoint.invoiceValue}
-          expenseValue={dataPoint.expenseValue}
-          maxValue={totalMax}
-          expenseMaxValue={expenseTotalMax}
-          label={dataPoint.label}
-          isDarkMode={props.isDarkMode}
-        />
-      ))}
+      <div className="flex justify-end gap-4 px-2">
+        <div className="flex items-center gap-2">
+          <FontAwesomeIcon
+            icon={faSquare}
+            className="text-brightPurple"
+          ></FontAwesomeIcon>
+          <p className="text-[#bac0e1] text-xs">Invoices</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <FontAwesomeIcon
+            icon={faSquare}
+            className="text-[#3b278b]"
+          ></FontAwesomeIcon>
+          <p className="text-[#bac0e1] text-xs">Expenses</p>
+        </div>
+      </div>
+
+      <div className="h-full flex justify-around">
+        {props.dataPoints.map(dataPoint => (
+          <ChartBar
+            key={dataPoint.label}
+            value={dataPoint.invoiceValue}
+            expenseValue={dataPoint.expenseValue}
+            maxValue={totalMax}
+            expenseMaxValue={expenseTotalMax}
+            label={dataPoint.label}
+            isDarkMode={props.isDarkMode}
+          />
+        ))}
+      </div>
     </div>
   );
 }
